22ade84afc0160d43923011c98e94fdf25c59138
Eduardo Igor <eduardoigor019@gmail.com>
2024-10-21 20:40:36 -0300

    Remove "contato" and 'sobre"

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Eduardo Igor',
     })
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
