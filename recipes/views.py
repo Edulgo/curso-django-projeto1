b3799b5ad40ed4ccd2128b70eb7430375b2145bd
Eduardo Igor <eduardoigor019@gmail.com>
2024-10-28 10:58:32 -0300

    new views

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Eduardo Igor',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Eduardo Igor',
+    })
